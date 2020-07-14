c3b5e259b806bb8ac38ae628ad4e92e2471cde95
huhangfei <huhangfei@yiche.com>
2020-07-14 19:06:26 +0800

    添加ScriptSearch

---
+++ src/NestTest/SerchTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nest;
+using Nest7Demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestTest
+{
+    [TestClass]
+    public class SerchTest
+    {
+        [TestMethod]
+        public void TestScriptSearch()
+        {
+            IElasticClient client = EsHelper.CreateElasticClient();
+            client.Search<TestModel>(s => s.Query(q => q.Bool(b => b.Must(m => m.Script(st => st.Script(spt => spt.Source("").Lang("")))))));
+        }
+        
+    }
+}
